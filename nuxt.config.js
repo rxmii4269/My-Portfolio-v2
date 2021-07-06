cb14922b623f28155ca393f450ea529bf51be203
Romaine Murray <romaine@knightfoxapps.com>
2021-07-05 21:19:31 -0500

    added description

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Learn about my endeavours as a developer and what I have built' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
