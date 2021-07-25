e8f1186972849de0c4b3a9625bf8dfdf8cfac771
Romaine Murray <romaine.murray@mymona.uwi.edu>
2021-07-25 16:21:29 -0500

    i give up

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ import customTheme from './custom-theme'
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: true,
+  ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'server',
